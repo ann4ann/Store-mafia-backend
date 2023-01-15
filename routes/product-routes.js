b8d5ff9d6a3a2c4d6cedcba0039bb85f032ac634
ilya <ilya123558ilya@gmail.com>
2023-01-15 14:37:43 +0300

    add path product

--- routes/product-routes.js
+++ routes/product-routes.js
@@ -3,11 +3,18 @@ import {
   addProduct,
   getAllProducts,
   updateProduct,
+  getProduct,
+  deleteProduct,
+  getProductsByCategory
 } from "../controllers/product-controller";
 const productRouter = express.Router();
 
 productRouter.get("/", getAllProducts);
+productRouter.get("/category/:category", getProductsByCategory);
+productRouter.get("/:id", getProduct);
 productRouter.post("/add", addProduct);
 productRouter.put("/update/:id", updateProduct);
+productRouter.delete("/delete/:id", deleteProduct);
 
 export default productRouter;
+ 
